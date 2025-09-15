1976d23b1c1c61ec326fa4e9a88f1ec606477716
Rkncodes <rajvinderkaurpersonal@gmail.com>
2025-09-16 05:21:46 +0530

    Update ThemeContext.jsx
    With this feature i have added dark mode toggled theme using tailwind css

--- frontend/src/contexts/ThemeContext.jsx
+++ frontend/src/contexts/ThemeContext.jsx
@@ -2,7 +2,7 @@ import React, { createContext, useContext, useState, useEffect } from 'react';
 
 const ThemeContext = createContext();
 
-export const useTheme = () => {
+export const useTheme = () =>  {
   const context = useContext(ThemeContext);
   if (!context) {
     throw new Error('useTheme must be used within a ThemeProvider');
